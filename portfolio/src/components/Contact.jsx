7daf82c1d5a0c83c7c3a2fb7f4be0e714de23b84
BrianTaylor <bataylor135@gmail.com>
2024-03-25 21:58:34 -0400

    ...

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -66,38 +66,36 @@ return (
         </div>
     
     <div className="contact-content">
-    <div className='row'>
-            <div className='col'>
-        <div className="card-flipper" onClick={() => handleFlip(2)}>
-        <div className={`card-content ${flippedId === 2 ? 'flipped' : ''}`}>
+    <div className="contact-info">
+    <div className="contact-card-extra">
+        <div className="contact-card-flipper" onClick={() => handleFlip(2)}>
+        <div className={`contact-card-content ${flippedId === 2 ? 'flipped' : ''}`}>
                     {/* Front face of the card */}
-    <div className="card-face card-face-front">
+    <div className="contact-card-face card-face-front">
     <h2>Get in Touch</h2>
     <p>Feel free to reach out to me with any questions or comments. I'm always looking for new opportunities.</p>
     <i className="fas fa-chevron-circle-right"></i>
     </div>
     {/* Back face of the card */}
-    <div className="card-face card-face-back">
+    <div className="contact-card-face card-face-back">
     <h2>Contact Me</h2>
     <p><i className="fas fa-envelope"></i></p>
     <p><i className="fas fa-map-marker-alt"></i></p>
     <p><i className="fas fa-phone"></i></p>
+    </div>
+    </div>
+    </div>
 
+    </div>
     
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div className='col'>
-    <div className="card-flipper" onClick={() => handleFlip(3)}>
-    <div className={`card-content ${flippedId === 3 ? 'flipped' : ''}`}>
-    <div className="card-face card-face-front">
+    <div className="contact-card-flipper" onClick={() => handleFlip(3)}>
+    <div className={`contact-card-content ${flippedId === 3 ? 'flipped' : ''}`}>
+    <div className="contact-card-face card-face-front">
     <h2>Connect with Me</h2>
     <p>Find me on LinkedIn and GitHub to see more of my work and experience.</p>
     <i className="fas fa-chevron-circle-right"></i>
     </div>
-    <div className="card-face card-face-back">
+    <div className="contact-card-face card-face-back">
     <h2>Connect with Me</h2>
     <div className="contact-links">
     <p><i className="fab fa-linkedin"></i>: <a href={contact.linkedin}>LinkedIn</a></p>
@@ -107,12 +105,17 @@ return (
     </div>
     </div>
     </div>
+    
 
-    <div className='col'>
-        <div className="contact-form-card">
-            <h2>Contact Form</h2>
+
+
+
+
+
+        <div className="contact-form-container">
                 <p>Send me a message using the form below:</p>
-        </div>
+  
+        <div className="contact-card">
         <form id="contact-form" name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}> 
             <div>
                 <label htmlFor="name">Name:</label><br />
@@ -138,6 +141,8 @@ return (
     {formStatus.error && <p>Sorry, there was a problem with your submission. Please try again, or contact me through LinkedIn or GitHub.</p>}
     </div>
     </div>
+    </div>
+
 </>
 
 );
--- portfolio/src/components/ProjectItem.jsx
+++ portfolio/src/components/ProjectItem.jsx
@@ -28,6 +28,7 @@ const ProjectItem = ({ project }) => {
 
   return (
 <>
+{/* row 1 */}
   <h2 className='project-title'>  {project.title}</h2>
 <div className="project-item" style={{ 
   display: 'flex', 
@@ -35,63 +36,51 @@ const ProjectItem = ({ project }) => {
   position: 'relative'  
 }}>  
 
-      <div className='card' style={{ width: '50%' }}>
-      <p>{project.description}</p>
-  <p>Technologies: {project.technologies.join(', ')}</p>
-  <p>Year: {project.year}</p>
-  <p>Challenges: {project.challenges}</p>
-  <p>Solutions: {project.solution}</p>
-  <a
-    href={project.link}
-    target="_blank"
-    rel="noopener noreferrer"
-    style={{
-      display: 'inline-block',
-      textDecoration: 'none',
-      padding: '10px 15px',
-      margin: '5px',
-      borderRadius: '2px',
-      backgroundColor: '#007bff',
-      color: 'white',
-      cursor: 'pointer'
-    }}
-  >
-    Check out the project
-  </a>
-    <a
-    href={project.github}
-    target="_blank"
-    rel="noopener noreferrer"
-    style={{
-      display: 'inline-block',
-      textDecoration: 'none',
-      padding: '10px 15px',
-      margin: '5px',
-      borderRadius: '2px',
-      backgroundColor: 'black',
-      color: 'white',
-      cursor: 'pointer'
-    }}
-  >
-    Check out the code
-  </a>
-
-
-
+<div className='project-card' style={{ backgroundColor: '#242424', color: "white", }}>
+  <div className="project-card-header">
+    <p className="project-card-title">{project.description}</p>
   </div>
 
-        <div style={{ width: '50%', position: 'relative', borderRadius: '10px', overflow: 'hidden' }}>
+  <div className="card-tech">
+  <h4>Technologies</h4>
+    <p className="project-card-tech">{project.technologies.join(' - ')}</p>
+  </div>
+  <div className="card-body">
+    <div className="card-technologies">
+        <h4>Challenges</h4>
+        <p>{project.challenges}</p>
+      </div>
+    <div className="card-info">
+      <div className="card-solutions">
+        <h4>Solutions</h4>
+        <p>{project.solution}</p>
+      </div>
+    </div>
+  </div>
+  <div className="project-card-footer">
+    <a href={project.link} target="_blank" rel="noopener noreferrer" className="card-link project-link">Check out the project</a>
+    <a href={project.github} target="_blank" rel="noopener noreferrer" className="card-link code-link">Check out the code</a>
+  </div>
+</div>
+
+{/* row 2 */}
+ 
+<div className="project-item" style={{ width: '100%', position: 'relative', borderRadius: '10px', overflow: 'hidden' }}>
     <ModelViewer modelUrl={project.model} />
     {isVideoPlaying && (
-      <div style={{ position: 'absolute', top: 0, left: 0 }}>
-        <video id="project-video" width="704" height="396" controls>
+       <div style={{ position: 'absolute', top: 10, left: 0, width: '100%', maxHeight: '100%', overflow: 'hidden' }}> 
+        <video id="project-video" width="96%" height="396" controls>
           <source src={project.videoUrl} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
     )}
+        {!isVideoPlaying && (
         <button onClick={handleVideoPlay}><i className="fas fa-play"></i> Play</button>
+        )}
+        {isVideoPlaying && (
         <button onClick={handleVideoStop}><i className="fas fa-stop"></i> Stop</button>
+      )}
       </div>
   </div>
   </>
