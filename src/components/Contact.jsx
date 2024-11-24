10932bb933f207811f1dcb33d95cd8ddf5efb292
BrianTaylor <bataylor135@gmail.com>
2024-11-23 21:30:25 -0500

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,93 +13,88 @@ const handleFlip = (id) => {
 return (
     <>
     {/* Header */}
-    <div className="projects-header text-center py-6">
-        <div
-        className="glitch font-bold text-3xl md:text-4xl"
-        data-text="Contact"
-        >
-        Contact
-        </div>
-    </div>
-
-    {/* Main Contact Section */}
+<div className="projects-header text-center py-6">
     <div
-        id="contact"
-        className="contact-content grid gap-6 px-4 py-10 bg-[#242424] rounded-lg text-white shadow-lg md:grid-cols-2"
+    className="glitch font-bold text-3xl md:text-4xl"
+    data-text="Contact"
     >
-    {/* Contact Info Section */}
+    Contact
+    </div>
+</div>
+
+{/* Main Contact Section */}
+<div className="contact-content grid gap-6 px-4 py-10 bg-[#242424] rounded-lg text-white shadow-lg md:grid-cols-2">
+{/* Contact Info Section */}
     <div className="contact-info flex flex-col items-center gap-6 md:row-span-2">
-    {/* Card 1: Get in Touch */}
-    <ContactCards
+        {/* Card 1: Get in Touch */}
+        <ContactCards
         id={2}
         flippedId={flippedId}
         handleFlip={handleFlip}
         frontContent={
         <div className="flex flex-col items-center justify-center h-full gap-4 px-4 text-center">
             <h2 className="text-lg font-semibold">Get in Touch</h2>
-            <p className="text-gray-300">
-            Reach out using the contact form below. I’m always excited to
-            connect and explore new opportunities!
-            </p>
-        </div>
-        }
-        backContent={
-        <div className="flex flex-col items-center justify-center h-full gap-4 px-4 text-center">
-            <h2 className="text-lg font-semibold">Let’s Collaborate</h2>
-            <p className="text-gray-300">
-            Use the form or find me on LinkedIn and GitHub. Let’s build
-            something awesome together!
-            </p>
-        </div>
-        }
-    />
-
-        {/* Card 2: Connect with Me */}
-        <ContactCards
-            id={3}
-            flippedId={flippedId}
-            handleFlip={handleFlip}
-            frontContent={
-            <div className="flex flex-col items-center justify-center h-full gap-4 px-4 text-center">
-                <h2 className="text-lg font-semibold">Connect with Me</h2>
                 <p className="text-gray-300">
+                    Reach out using the contact form below. I’m always excited to
+                    connect and explore new opportunities!
+                </p>
+        </div>
+    }
+            backContent={
+            <div className="flex flex-col items-center justify-center h-full gap-4 px-4 text-center">
+                <h2 className="text-lg font-semibold">Let’s Collaborate</h2>
+                    <p className="text-gray-300">
+                    Use the form or find me on LinkedIn and GitHub. Let’s build something awesome together!
+                    </p>
+            </div>
+    }
+    />
+    {/* Card 2: Connect with Me */}
+    <ContactCards
+    id={3}
+    flippedId={flippedId}
+    handleFlip={handleFlip}
+    frontContent={
+        <div className="flex flex-col items-center justify-center h-full gap-4 px-4 text-center">
+            <h2 className="text-lg font-semibold">Connect with Me</h2>
+            <p className="text-gray-300">
                 Find me on LinkedIn and GitHub to see more of my work and
                 experience.
-                </p>
-                <i className="fas fa-chevron-circle-right text-lg mt-2 transition-transform duration-300 hover:rotate-90 hover:text-[#b5b5b5]"></i>
-            </div>
-            }
-            backContent={
-            <div className="contact-links flex flex-col items-center gap-5">
-                <p className="flex items-center">
+            </p>
+            <i className="fas fa-chevron-circle-right text-lg mt-2 transition-transform duration-300 hover:rotate-90 hover:text-[#b5b5b5]"></i>
+        </div>
+    }
+    backContent={
+        <div className="contact-links flex flex-col items-center gap-5">
+            <p className="flex items-center">
                 <i className="fab fa-linkedin mr-2"></i>
                 <a
-                    href={contact.linkedin}
-                    className="text-white text-base hover:text-[#008dd4] transition-colors duration-300"
+                href={contact.linkedin}
+                className="text-white text-base hover:text-[#008dd4] transition-colors duration-300"
                 >
-                    LinkedIn
+                LinkedIn
                 </a>
-                </p>
-                <p className="flex items-center">
+            </p>
+            <p className="flex items-center">
                 <i className="fab fa-github mr-2"></i>
                 <a
-                    href={contact.github}
-                    className="text-white text-base hover:text-[#008dd4] transition-colors duration-300"
+                href={contact.github}
+                className="text-white text-base hover:text-[#008dd4] transition-colors duration-300"
                 >
-                    GitHub
+                GitHub
                 </a>
-                </p>
-            </div>
-            }
-        />
+            </p>
         </div>
-
-        {/* Form Section */}
-        <div className="contact-form border ">
-  <Form />
+    }
+    />
 </div>
+
+  {/* Form Section */}
+    <div className="contact-form border p-4 rounded-lg bg-white text-black">
+        <Form />
     </div>
-    </>
+</div>
+</>
 );
 };
 
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -6,7 +6,7 @@ const ProjectItem = ({ project }) => {
   const modelElement = useRef(null); 
   const [isInView, setInView] = useState(false);
   const projectRef = useRef();
- 
+
   useEffect(() => {
     const currentRef = projectRef.current;
     const observer = new IntersectionObserver(([entry]) => {
@@ -60,85 +60,84 @@ const ProjectItem = ({ project }) => {
   position: 'relative'  
 }}>  
 
-<div className="project-card bg-gray-900 text-white rounded-lg shadow-xl overflow-hidden transition-all duration-300 hover:shadow-2xl">
-  <div className="p-2">
-    <div className="mb-4">
-      <p className="text-xl font-bold text-white mb-2">{project.title}</p>
-      <p className="text-gray-300 text-base">{project.description}</p>
-    </div>
-    
-    <div className="space-y-4 border-t border-gray-700 pt-2">
-      <div className="card-challenges">
-        <h5 className="text-md uppercase font-semibold text-gray-200 mb-2">Challenges</h5>
-        <p className="text-gray-300 text-base">{project.challenges}</p>
+  <div className="project-card bg-gray-900 text-white rounded-lg shadow-xl overflow-hidden transition-all duration-300 hover:shadow-2xl">
+    <div className="p-2">
+      <div className="mb-4">
+        <p className="text-xl font-bold text-white mb-2">{project.title}</p>
+        <p className="text-gray-300 text-base">{project.description}</p>
       </div>
       
-      <div className="card-solutions">
-        <h5 className="text-md uppercase font-semibold text-gray-200 mb-2">Solution</h5>
-        <p className="text-gray-300 text-base">{project.solution}</p>
-      </div>
-    </div>
-  </div>
-
-  
-  <div className="project-card-footer flex justify-between items-center">
-  <a 
-    href={project.github} 
-    target="_blank" 
-    rel="noopener noreferrer" 
-    className="flex text-center card-link code-link duration-200"
-  > 
-  <i className="fab fa-github mr-2 icon"></i>
-    GitHub
-  </a>
-  <a 
-    href={project.link} 
-    target="_blank" 
-    rel="noopener noreferrer" 
-    className="flex items-center card-link project-link duration-200"
-  >
-    <i className="fab fa-chrome mr-2 text-white text-3xl"></i>
-    Live Project
-  </a>
-</div>
-</div>
-
-
-<div className="project-item w-full relative rounded-lg overflow-hidden">
-<div ref={projectRef}
-  style={{ minHeight: '400px' }} 
->
-  {isInView ? (
-    <ModelViewer modelUrl={project.model} />
-  ) : (
-    <div style={{ height: '100%' }}></div> 
-  )}
-</div>
-    {isVideoPlaying && (
-      <div className="absolute top-20 left-5 md:top-6 md:left-3 w-[90%] md:w-full max-h-full overflow-hidden" >
-          <video 
-              id="project-video" className="w-full md:w-[96%] max-h-[50vh] md:max-h-[396px] rounded-lg" controls>
-              <source src={project.videoUrl} type="video/mp4" />
-              Your browser does not support the video tag.
-          </video>
-      </div>
-    )}
-  {!isVideoPlaying && project.videoUrl && (
-    <button className="video-button" onClick={handleVideoPlay}>
-      <i className="fas fa-play"></i> Play
-    </button>
-  )}
-  {isVideoPlaying && (
-    <button className="video-button" onClick={handleVideoStop}>
-      <i className="fas fa-stop"></i> Stop
-    </button>
-  )}
+      <div className="space-y-4 border-t border-gray-700 pt-2">
+        <div className="card-challenges">
+          <h5 className="text-md uppercase font-semibold text-gray-200 mb-2">Challenges</h5>
+          <p className="text-gray-300 text-base">{project.challenges}</p>
         </div>
+        
+        <div className="card-solutions">
+          <h5 className="text-md uppercase font-semibold text-gray-200 mb-2">Solution</h5>
+          <p className="text-gray-300 text-base">{project.solution}</p>
+        </div>
+      </div>
     </div>
-    <div className="divider-block"></div>
-    </>
-    );
-  };
+
+    
+    <div className="project-card-footer flex justify-between items-center">
+      <a 
+        href={project.github} 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="flex text-center card-link code-link text-sm md:text-base duration-200"
+      > 
+      <i className="fab fa-github mr-2 icon"></i>
+        GitHub
+      </a>
+      <a 
+        href={project.link} 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="flex items-center card-link project-link text-sm md:text-base duration-200"
+      >
+        <i className="fab fa-chrome mr-2 text-white text-3xl"></i>
+        Live Project
+      </a>
+      </div>`
+    </div>
+
+    <div className="project-item w-full relative rounded-lg overflow-hidden">
+      <div ref={projectRef}
+        style={{ minHeight: '400px' }} 
+      >
+        {isInView ? (
+          <ModelViewer modelUrl={project.model} />
+        ) : (
+          <div style={{ height: '100%' }}></div> 
+        )}
+      </div>
+          {isVideoPlaying && (
+            <div className="absolute top-20 left-5 md:top-6 md:left-3 w-[90%] md:w-full max-h-full overflow-hidden" >
+                <video 
+                    id="project-video" className="w-full md:w-[96%] max-h-[50vh] md:max-h-[396px] rounded-lg" controls>
+                    <source src={project.videoUrl} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+          )}
+        {!isVideoPlaying && project.videoUrl && (
+          <button className="video-button" onClick={handleVideoPlay}>
+            <i className="fas fa-play"></i> Play
+          </button>
+        )}
+        {isVideoPlaying && (
+          <button className="video-button" onClick={handleVideoStop}>
+            <i className="fas fa-stop"></i> Stop
+          </button>
+        )}
+    </div>
+</div>
+      <div className="divider-block"></div>
+      </>
+      );
+    };
 
 
 export default ProjectItem;
